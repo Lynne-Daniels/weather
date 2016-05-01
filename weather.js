c6575b1211fb90339274a20a407fc01b487d2321
Lynne Daniels <lynne33477@gmail.com>
2016-04-30 23:15:31 -0400

    all done

--- weather.js
+++ weather.js
@@ -1,118 +1,100 @@
 /* Get the weather and update the page
-
- * 
- * 
- * API CALL:  api.openweathermap.org/data/2.5/weather?zip=33418,us&appid=9edbccf035e35ac9403515d616122550 //my API key
- * SEE CODES:  http://openweathermap.org/current#current_JSON
-
- * 
+ *
+ * API CALL:  api.openweathermap.org/data/2.5/weather?zip=33418,us&appid=9edbccf035e35ac9403515d616122550 //
+ * Documentation:  http://openweathermap.org/current#current_JSON
  */
-
-
-
-
-
-//https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+// Get user's location https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
 var options = {
-  enableHighAccuracy: false,
-  timeout: 5000,
-  maximumAge: 0
- 
+	enableHighAccuracy: false,
+	timeout: 5000,
+	maximumAge: 0
 };
 
-function success(pos) {
-  var crd = pos.coords;
-
-  console.log('Your current position is:');
-  console.log('Latitude : ' + crd.latitude);
-  console.log('Longitude: ' + crd.longitude);
-  console.log('More or less ' + crd.accuracy + ' meters.');
-  
- // $("#users-coords").html("latitude: " + crd.latitude + " longitude: " + crd.longitude);
-  getWeather();
+function success(pos)
+{
+	var crd = pos.coords;
+	console.log('Your current position is:');
+	console.log('Latitude : ' + crd.latitude);
+	console.log('Longitude: ' + crd.longitude);
+	console.log('More or less ' + crd.accuracy + ' meters.');
+	// $("#users-coords").html("latitude: " + crd.latitude + " longitude: " + crd.longitude);
+	getWeather();
 };
 
-function error(err) {
-  console.warn('ERROR(' + err.code + '): ' + err.message);
+function error(err)
+{
+	console.warn('ERROR(' + err.code + '): ' + err.message);
 };
-
 navigator.geolocation.getCurrentPosition(success, error, options);
-
 //https://learn.jquery.com/ajax/jquery-ajax-methods/  Get the weather data
-function getWeather() {$.ajax({
- 
-    // The URL for the request
-    url: "http://api.openweathermap.org/data/2.5/weather?lat=26.861214099999998&lon=-80.1372164&appid=9edbccf035e35ac9403515d616122550",
- 
-    // The data to send (will be converted to a query string)
-   // data: {
-    //    id: 123
-  //  },
- 
-    // Whether this is a POST or GET request
-    type: "GET",
- 
-    // The type of data we expect back
-    dataType : "json",
-})
-  // Code to run if the request succeeds (is done);
-  // The response is passed to the function
-  .done(function( json ) {
-     $("#users-coords").html(json.name);
-     $( "#current-weather" ).text( json.weather[0].main + " " + json.weather[0].description);
-     // get temperatue in degrees Kelvin, use button group to control conversions to Farenheit and Centigrade
-     temperatureKelvin = Math.floor(json.main.temp);
-     
-     $("#temperature").text(json.main.temp);
-     $("#wind").text("Wind Speed:  " + json.wind.speed + " Direction:  " + json.wind.deg + " degrees" );
-     //transform a number into a date object
-     var sunriseDT = new Date(json.sys.sunrise*1000);
-     var sunsetDT = new Date(json.sys.sunset*1000);
-     
-     $("#sunrise-sunset").html("Sunrise: "+ sunriseDT.toLocaleString() + "<br> Sunset: " + (sunsetDT.toLocaleString()));
-    console.log("epoch to string " + sunriseDT.toLocaleString());
-    //console.log("length "+ json.properties);
-    //console.log("json" + json.data);
-    var weatherIcon = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
-    $("#weather-image").html('<img src = '+ weatherIcon + ' alt = "weather related image"><img>');
-    console.log(weatherIcon + " icon" + json.weather[0].icon);
-    
-  })
-  // Code to run if the request fails; the raw request and
-  // status codes are passed to the function
-  .fail(function( xhr, status, errorThrown ) {
-    alert( "Sorry, there was a problem!" );
-    console.log( "Error: " + errorThrown );
-    console.log( "Status: " + status );
-    console.dir( xhr );
-  })
-  // Code to run regardless of success or failure;
-  .always(function( xhr, status ) {
-    //alert( "The request is complete!" );
-  });
+function getWeather()
+	{
+		$.ajax(
+			{
+				// The URL for the request
+				url: "http://api.openweathermap.org/data/2.5/weather?lat=26.861214099999998&lon=-80.1372164&appid=9edbccf035e35ac9403515d616122550&units=imperial",
+				// Whether this is a POST or GET request
+				type: "GET",
+				// The type of data we expect back
+				dataType: "json",
+			})
+			// Code to run if the request succeeds (is done);
+			// The response is passed to the function
+			.done(function(json)
+			{
+				$("#users-coords").html(json.name);
+				$("#current-weather").text(json.weather[0].main + " " + json.weather[0].description);
+				// get temperatue in degrees Farenheit, use button group to control conversion to Centigrade
+				temperatureFarenheit = Math.round(json.main.temp);
+				$("#temperature").text(temperatureFarenheit + " °F");
+				$("#wind").text("Wind Speed:  " + Math.round(json.wind.speed) +
+					" Direction:  " + degToCompass(Math.round(json.wind.deg)));
+				//transform a number into a date object
+				var sunriseDT = new Date(json.sys.sunrise * 1000);
+				var sunsetDT = new Date(json.sys.sunset * 1000);
+				$("#sunrise-sunset").html("Sunrise: " + sunriseDT.toLocaleString() +
+					"<br> Sunset: " + (sunsetDT.toLocaleString()));
+				console.log("epoch to string " + sunriseDT.toLocaleString());
+				//console.log("length "+ json.properties);
+				//console.log("json" + json.data);
+				var weatherIcon = "http://openweathermap.org/img/w/" + json.weather[0].icon +
+					".png";
+				$("#weather-image").html('<img src = ' + weatherIcon +
+					' alt = "weather related image"><img>');
+				console.log(weatherIcon + " icon" + json.weather[0].icon);
+			})
+			// Code to run if the request fails; the raw request and
+			// status codes are passed to the function
+			.fail(function(xhr, status, errorThrown)
+			{
+				alert("Sorry, there was a problem!");
+				console.log("Error: " + errorThrown);
+				console.log("Status: " + status);
+				console.dir(xhr);
+			})
+			// Code to run regardless of success or failure;
+			.always(function(xhr, status)
+			{
+				//alert( "The request is complete!" );
+			});
+	}
+	//http://stackoverflow.com/questions/7490660/converting-wind-direction-in-angles-to-text-words
+
+function degToCompass(num)
+{
+	var val = Math.floor((num / 22.5) + 0.5);
+	var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+	return arr[(val % 16)];
 }
-//assign weather appropriate icon
-//var weatherIcon = "http://openweathermap.org/img/w/" + weatherIcon;
-
-
-
-// TODO case src is "http://openweathermap.org/img/w/10d.png"
-//event handlers for buttons
- //clicked button color indicates initial units
-var  temperatureKelvin = 0;
-$(document).ready(function(){
-	
-    $('#celcius').on('click', function(e)
-    {
-		$("#temperature").text(temperatureKelvin - 273 + " C");
-    });
-        $('#farenheit').on('click', function(e)
-    {
-		$("#temperature").text(Math.floor(temperatureKelvin *9/5 - 460) + " F");
-    });
-        $('#kelvin').on('click', function(e)
-    {
-		$("#temperature").text(temperatureKelvin + " K");
-    });
-
+var temperatureKelvin = 0;
+$(document).ready(function()
+{
+	$('#celcius').on('click', function(e)
+	{
+		$("#temperature").text(Math.round((temperatureFarenheit - 32) * 5 / 9) +" °C");
+	});
+	$('#farenheit').on('click', function(e)
+	{
+		$("#temperature").text(temperatureFarenheit + " °F");
+	});
 }); //doc ready
